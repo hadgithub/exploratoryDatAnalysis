715def60619cfec6ec3aeda6101564daa20cfd3f
HAD <hdiosa@udistrital.edu.co>
2016-01-25 00:58:04 -0500

    Add first and second parts

--- plot1.R
+++ plot1.R
@@ -1,6 +1,7 @@
 plot1 <- function(dataFile){
+        library(dplyr)
         cons <- read.csv(dataFile,sep=";",na.strings="?")
-        consProy1 <- filter(cons,as.Date(Date) >= as.Date("01/02/2007") & as.Date(Date) <= as.Date("02/02/2007"))
+        consProy1 <- filter(cons,as.Date(Date) >= as.Date("02/01/2007") & as.Date(Date) <= as.Date("02/02/2007"))
         hist(consProy1$Global_active_power,col="red",main="Active Global Power",xlab="Global Active Power(kilowatts)",ylab="Frequency")
         dev.copy(png, file = "plot1.png", width = 480, height=480)
         dev.off()
--- plot2.R
+++ plot2.R
@@ -1,10 +1,13 @@
 plot2 <- function(dataFile){
+        library(dplyr)
+        library(ggplot2)
         cons <- read.csv(dataFile,sep=";",na.strings="?")
-        consProy1 <- filter(cons,as.Date(Date) >= as.Date("01/02/2007") & as.Date(Date) <= as.Date("02/02/2007"))
+        consProy1 <- filter(cons,as.Date(Date) >= as.Date("02/01/2007") & as.Date(Date) <= as.Date("02/02/2007"))
         cons4DayComp <- complete.cases(consProy1)
         consPlot2 <- consProy1[cons4DayComp,][,]
         consPlot4DayFinal<-select(consPlot2,Date,Time,Global_active_power)
-        with(consPlot4DayFinal,qplot(Date,Global_active_power,ylim=rng))
+        consPlot4Day<-group_by(consPlot4DayFinal,Date)
+        with(consPlot4Day,qplot(Date,Global_active_power,ylim=rng))
         dev.copy(png, file = "plot2.png", width = 480, height=480)
         dev.off()
 }
