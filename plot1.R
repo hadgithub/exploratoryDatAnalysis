a37a5ea55feaa420b401cb1639e2ef77cca2700a
HAD <hdiosa@udistrital.edu.co>
2016-01-25 07:47:49 -0500

    Add first part O.K.

--- plot1.R
+++ plot1.R
@@ -1,7 +1,10 @@
 plot1 <- function(dataFile){
         library(dplyr)
         cons <- read.csv(dataFile,sep=";",na.strings="?")
-        consProy1 <- filter(cons,as.Date(Date) >= as.Date("02/01/2007") & as.Date(Date) <= as.Date("02/02/2007"))
+        cons4DayComp <- complete.cases(cons)
+        conSiNas <- cons[cons4DayComp,][,]
+        conSiNas<-mutate(conSiNas,Date = as.Date(Date,format="%d/%m/%Y"))
+        consProy1 <- filter(conSiNas, Date >= as.Date("01/02/2007",format="%d/%m/%Y") & Date <= as.Date("02/02/2007",format="%d/%m/%Y"))
         hist(consProy1$Global_active_power,col="red",main="Active Global Power",xlab="Global Active Power(kilowatts)",ylab="Frequency")
         dev.copy(png, file = "plot1.png", width = 480, height=480)
         dev.off()
