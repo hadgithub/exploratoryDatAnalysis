e5d7c1c92b824fb484533cdcd1a786cf5d2ad7aa
HAD <hdiosa@udistrital.edu.co>
2016-01-25 11:50:12 -0500

    Add second part O.K.

--- plot2.R
+++ plot2.R
@@ -2,12 +2,14 @@ plot2 <- function(dataFile){
         library(dplyr)
         library(ggplot2)
         cons <- read.csv(dataFile,sep=";",na.strings="?")
-        consProy1 <- filter(cons,as.Date(Date) >= as.Date("02/01/2007") & as.Date(Date) <= as.Date("02/02/2007"))
-        cons4DayComp <- complete.cases(consProy1)
-        consPlot2 <- consProy1[cons4DayComp,][,]
-        consPlot4DayFinal<-select(consPlot2,Date,Time,Global_active_power)
+        cons4DayComp <- complete.cases(cons)
+        conSiNas <- cons[cons4DayComp,][,]
+        conSiNas<-mutate(conSiNas,Date = as.Date(Date,format="%d/%m/%Y"))
+        consProy1 <- filter(conSiNas, Date >= as.Date("01/02/2007",format="%d/%m/%Y") & Date <= as.Date("02/02/2007",format="%d/%m/%Y"))
+        consPlot4DayFinal<-select(consProy1,Date,Time,Global_active_power)
+        consPlot4DayFinal <- mutate(consPlot4DayFinal,Date=weekdays(Date))
         consPlot4Day<-group_by(consPlot4DayFinal,Date)
-        with(consPlot4Day,qplot(Date,Global_active_power,ylim=rng))
+        xyplot(Global_active_power~Time | Date, data = consPlot4Day, layout = c(2, 1),auto.key = list(space = "right", points = FALSE, lines = TRUE),plot.points=TRUE,type="a")
         dev.copy(png, file = "plot2.png", width = 480, height=480)
         dev.off()
 }
