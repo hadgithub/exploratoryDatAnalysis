0053cbc7f7e371dead359aa92079ca25ecc318bb
Henry Alberto Diosa <dhenry@une.net.co>
2016-02-23 09:09:24 -0500

    Added files via upload

--- plot2.R
+++ plot2.R
@@ -1,16 +1,17 @@
 plot2 <- function(dataFile){
                 library(dplyr)
-                library(ggplot2)
-                library(lattice)
                 cons <- read.csv(dataFile,sep=";",na.strings="?")
                 cons4DayComp <- complete.cases(cons)
                 conSiNas <- cons[cons4DayComp,][,]
                 conSiNas<-mutate(conSiNas,Date = as.Date(Date,format="%d/%m/%Y"))
                 consProy1 <- filter(conSiNas, Date >= as.Date("01/02/2007",format="%d/%m/%Y") & Date <= as.Date("02/02/2007",format="%d/%m/%Y"))
                 consPlot4DayFinal<-select(consProy1,Date,Time,Global_active_power)
-                consPlot4DayFinal <- mutate(consPlot4DayFinal,Date=weekdays(Date))
-                consPlot4Day<-group_by(consPlot4DayFinal,Date)
-                xyplot(Global_active_power~Time | Date, data = consPlot4Day, layout = c(2, 1),auto.key = list(space = "right", points = FALSE, lines = TRUE),plot.points=TRUE,type="a")
+                consPlot4DayFinal <- mutate(consPlot4DayFinal,DateTime=format(as.POSIXct(paste(Date,Time)),na.rm=TRUE))
+                plot(as.POSIXct(consPlot4DayFinal$DateTime),consPlot4DayFinal$Global_active_power, type="l", col="black", xaxt = "n",xlab="Days", ylab="Global Active Power (kilowatts)")
+                DTmin = as.POSIXct(consPlot4DayFinal[1,4])
+                DTmax = as.POSIXct(consPlot4DayFinal[2880,4])
+                mid= DTmin + (DTmax-DTmin)/2 
+                axis(1, at=c(DTmin,mid,DTmax), labels=c("Thu","Fri","Sat"),tick = TRUE,col.axis="black")
                 dev.copy(png, file = "plot2.png", width = 480, height=480)
                 dev.off()
 }
